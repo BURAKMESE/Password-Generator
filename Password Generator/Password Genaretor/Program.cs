d4791abcc4ec72ab23ef236aa6a7b292f98544c0
BURAKMESE <62951821+BURAKMESE@users.noreply.github.com>
2024-04-18 02:07:31 +0300

    Add files via upload

---
+++ Password Generator/Password Genaretor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Password Length: ");
+        int uzunluk = Convert.ToInt32(Console.ReadLine());
+
+        string sifre = SifreOlustur(uzunluk);
+        Console.WriteLine("Generated Password: " + sifre);
+
+        Console.ReadKey();
+    }
+
+    static string SifreOlustur(int uzunluk)
+    {
+        const string karakterler = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+";
+        StringBuilder sb = new StringBuilder();
+
+        Random random = new Random();
+        for (int i = 0; i < uzunluk; i++)
+        {
+            int index = random.Next(karakterler.Length);
+            sb.Append(karakterler[index]);
+        }
+
+        return sb.ToString();
+    }
+}
